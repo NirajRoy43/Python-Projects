ff520e1792fb210439a61cd1ec3da5b21d6f312e
Niraj Kumar <149550225+NirajRoy43@users.noreply.github.com>
2024-06-08 20:49:52 +0530

    Update jpg2png.py

--- jpg-to-png/jpg2png.py
+++ jpg-to-png/jpg2png.py
@@ -1,11 +1,5 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from
-SyntaxError: invalid syntax
->>> from PIL import Image
->>> im = Image.open(r"C:\Users\hp\Pictures\signnn.jpg")
->>> im.save(r"C:\Users\hp\Pictures\new_sign.png")
->>> 
->>> 
->>> 
->>> 
+
+from PIL import Image
+im = Image.open(r"file_location")
+im.save(r"file_location")
+
