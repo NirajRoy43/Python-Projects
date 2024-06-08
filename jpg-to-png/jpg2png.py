2f93a0fa2bda8e14ea2a35ac8bbfd5f9c510b0af
Niraj Kumar <149550225+NirajRoy43@users.noreply.github.com>
2024-06-08 20:47:51 +0530

    Add files via upload

---
+++ jpg-to-png/jpg2png.py
@@ -0,0 +1,11 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from
+SyntaxError: invalid syntax
+>>> from PIL import Image
+>>> im = Image.open(r"C:\Users\hp\Pictures\signnn.jpg")
+>>> im.save(r"C:\Users\hp\Pictures\new_sign.png")
+>>> 
+>>> 
+>>> 
+>>> 
