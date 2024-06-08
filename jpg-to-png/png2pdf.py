2768232aebd0a4c62788162bdf85a16daf2069f6
Niraj Kumar <149550225+NirajRoy43@users.noreply.github.com>
2024-06-09 02:40:24 +0530

    Create png2pdf.py

---
+++ jpg-to-png/png2pdf.py
@@ -0,0 +1,5 @@
+from PIL import Image
+im=Image.open(r"Your_File_Location.png") # where your file is located
+im_1=im.convert('RGB') 
+(r"Your_File_Location.png")  # where you want to save your file
+
